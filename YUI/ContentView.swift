e5e943a7eefed64e466a0fc2d3de9b2f68e51a47
morikentiger <kenrur339@gmail.com>
2022-02-10 14:18:20 +0900

    Initial Commit

---
+++ YUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  YUI
+//
+//  Created by 森田健太 on 2022/02/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ YUI/YUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  YUIApp.swift
+//  YUI
+//
+//  Created by 森田健太 on 2022/02/10.
+//
+
+import SwiftUI
+
+@main
+struct YUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
