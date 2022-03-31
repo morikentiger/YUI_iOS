e25a77bc56ecf9e0021ea75fadcd07c2d274ab66
morikentiger <kenrur339@gmail.com>
2022-03-31 11:41:40 +0900

    あいづちを打つ機能追加　ボタンをイメージのみに変更

--- YUI/ContentView.swift
+++ YUI/ContentView.swift
@@ -20,121 +20,92 @@ struct ContentView: View {
     
     let voicePitch = 1.2
     let pauseTime = 1.0
-    @State var yuiSession = "おはようございます。けんたさん。"
-    @State var yuiPostSession :[String] = []
-    @State var usrSession :[String] = []
-    
-    @State var countConversation = 0
-    
-    
-
+    @State var yuiSession = "あなたのお話、聞かせてください。"
+//    @State var yuiSessions :[String] = []
+    var yuiSessions : Array<String> = ["そうなんだね","そうだよね","わかるよ","大変だったね","うんうん","それで？","そうなんだ","わかるよ","ふんふん","で？","それから？","うん","そうだね","それでどうなったの？","そうなんだね"]
     
     // 会話パターン条件分岐（ここがメインのアルゴリズムだよっ！）
     func talkPatternConditionalBranch(){
+        yuiSession = "あなたのお話、聞かせてください。"
         
-        
-        
-        
-        
-        
-        
-        if(self.speechRecorder.audioText.contains("Hey Siri") || self.speechRecorder.audioText.contains("OK Google")){
-          yuiSession = "あの女のほうがいいの？"
+        // ランダムであいづちを打つ
+        if(self.speechRecorder.audioText.utf8.count > 1){
+            yuiSession = yuiSessions.randomElement()!
         }
         
-        
-        // 発言を記録
-        usrSession.append(self.speechRecorder.audioText)
-        yuiPostSession.append(yuiSession)
-        
-        // 会話回数のカウント
-        countConversation += 1
-        
-        
+        if(self.speechRecorder.audioText.contains("答え") || self.speechRecorder.audioText.contains("教えて") || self.speechRecorder.audioText.contains("どう")){
+          yuiSession = "        ‣ YUIはあなたのお話を聞くことしかできません。答えを出す、選択をするのはあなた自身なのです。そのお手伝いができたら、YUIは本望です！それじゃダメかな？"
+        }
     }
     
     var body: some View {
+        VStack(alignment: .leading, spacing: 5) {
+            Text(self.speechRecorder.audioText)
+                .frame(maxWidth: .infinity, maxHeight: 400)
+            HStack() {
+                Spacer()
+                Button(action: {
+                    if(AVCaptureDevice.authorizationStatus(for: AVMediaType.audio) == .authorized &&
+                        SFSpeechRecognizer.authorizationStatus() == .authorized){
+                        self.showingAlert = false
+                        self.speechRecorder.toggleRecording()
+                        if !self.speechRecorder.audioRunning {
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 2.5) {
+                                
+                            }
+                        }
+                    }
+                    else{
+                        self.showingAlert = true
+                    }
+                })
+                {
+                    if !self.speechRecorder.audioRunning {
+                        Image(systemName: "mic")
+                            .font(.system(size: 60))
+                    } else {
+                        Image(systemName: "mic.fill")
+                            .font(.system(size: 60))
+                    }
+                }
+                .alert(isPresented: $showingAlert) {
+                    Alert(title: Text("マイクの使用または音声の認識が許可されていません"))
+                }
+                Spacer()
+            }
+            
+        }
+        .onAppear{
+            AVCaptureDevice.requestAccess(for: AVMediaType.audio) { granted in
+                OperationQueue.main.addOperation {
+                    
+                }
+            }
+            SFSpeechRecognizer.requestAuthorization { status in
+                OperationQueue.main.addOperation {
+                    //switch status {
+                    //    case .authorized:
+                    //
+                    //    default:
+                    //
+                    //}
+                }
+            }
+        }
         
         
-        Button("ここを押すとYUIが返事するよ！") {
+        Button {
             talkPatternConditionalBranch()
             
             let synthesizer = AVSpeechSynthesizer()
-//            let utterance = AVSpeechUtterance(string: "こんにちは。わたしはYUIです。")
             let utterance = AVSpeechUtterance(string: yuiSession)
-//            let utterance = AVSpeechUtterance(string: self.speechRecorder.audioText)
             utterance.voice = AVSpeechSynthesisVoice(language: "ja-JP")
             utterance.pitchMultiplier = Float(voicePitch)
             utterance.postUtteranceDelay = pauseTime
             synthesizer.speak(utterance)
-        }.padding()
-            .overlay(
-                RoundedRectangle(cornerRadius: 10)
-                    .stroke(Color.blue, lineWidth: 1))
-            
-        
-        ScrollView{
-            VStack(alignment: .leading, spacing: 5) {
-                HStack() {
-                    Spacer()
-                    Button(action: {
-                        if(AVCaptureDevice.authorizationStatus(for: AVMediaType.audio) == .authorized &&
-                            SFSpeechRecognizer.authorizationStatus() == .authorized){
-                            self.showingAlert = false
-                            self.speechRecorder.toggleRecording()
-                            if !self.speechRecorder.audioRunning {
-                                DispatchQueue.main.asyncAfter(deadline: .now() + 2.5) {
-                                    
-                                }
-                            }
-                        }
-                        else{
-                            self.showingAlert = true
-                        }
-                    })
-                    {
-                        if !self.speechRecorder.audioRunning {
-                            Text("ここを押してお話してね！")
-                                .padding()
-                                .overlay(
-                                    RoundedRectangle(cornerRadius: 10)
-                                        .stroke(Color.blue, lineWidth: 1))
-                        } else {
-                            Text("お話し終えたら押してね！")
-                                .padding()
-                                .overlay(
-                                    RoundedRectangle(cornerRadius: 10)
-                                        .stroke(Color.red, lineWidth: 1))
-                        }
-                    }
-                    .alert(isPresented: $showingAlert) {
-                        Alert(title: Text("マイクの使用または音声の認識が許可されていないよ"))
-                    }
-                    Spacer()
-                }
-                Text(self.speechRecorder.audioText)
-            }
-            .onAppear{
-                AVCaptureDevice.requestAccess(for: AVMediaType.audio) { granted in
-                    OperationQueue.main.addOperation {
-                        
-                    }
-                }
-                SFSpeechRecognizer.requestAuthorization { status in
-                    OperationQueue.main.addOperation {
-                        //switch status {
-                        //    case .authorized:
-                        //
-                        //    default:
-                        //
-                        //}
-                    }
-                }
-            }
-        }.padding(.vertical)
-        
-        List(usrSession, id: \.self) { usrSession in
-            Text(usrSession)
+        } label: {
+            Image(systemName: "mouth")
+                .font(.system(size: 60))
         }
     }
 }
